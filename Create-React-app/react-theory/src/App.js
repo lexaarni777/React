43e63e41f3c94965fadc683cbf32a1bfa41effc8
user <Lexaarni777@yandex.ru>
2020-02-22 14:30:50 +0300

    Конец 3 блока

--- Create-React-app/react-theory/src/App.js
+++ Create-React-app/react-theory/src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Car from './Car/Car'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = {
+        cars: [
+            { name: 'Mazda 3', year: '2010' },
+            { name: 'Mazda 6', year: '2019' }
+        ],
+        PageTittle: 'React component',
+        showCars: false
+    }
+
+
+    toggleCarsHandler = (newTittle) => {
+        
+        this.setState({
+            showCars: !this.state.showCars
+        })
+    }
+
+    onChangeName(name, index) {
+        const car = this.state.cars[index]
+        car.name = name
+        const cars = [...this.state.cars]
+        cars[index] = car
+        this.setState({
+            cars
+        })
+    }
+
+    deleteHandler(index) {
+        const cars = this.state.cars.concat()
+        cars.splice(index, 1)
+        this.setState({cars})
+    }
+
+    render() {
+        let cars = null
+
+        if (this.state.showCars) {
+            cars = this.state.cars.map((car, index) => {
+                return (
+                    <Car
+                        key={index}
+                        name={car.name}
+                        year={car.year}
+                        onDelete={this.deleteHandler.bind(this, index)}
+                        onChangeName={event => this.onChangeName(event.target.value, index)}
+                    />
+                )
+            })
+        }
+      return (
+          <div className="App">
+              <h1>{this.state.PageTittle}</h1>
+
+              <button onClick={this.toggleCarsHandler}>Toogle cars</button>
+
+              {cars}
+          </div>
+
+
+      );
+    }
 }
 
 export default App;
