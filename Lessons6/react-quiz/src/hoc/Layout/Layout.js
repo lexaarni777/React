fd38a23850aad5eced358703cc869b18d6eb6953
lexaarni777 <49690573+lexaarni777@users.noreply.github.com>
2020-03-28 01:03:36 +0300

    6 блок 5 занятие

--- Lessons6/react-quiz/src/hoc/Layout/Layout.js
+++ Lessons6/react-quiz/src/hoc/Layout/Layout.js
@@ -1,5 +1,5 @@
  import React, {Component} from 'react'
- import classes from './Layout/Layout.module.css'
+ import classes from './Layout.module.css'
 
  class Layout extends Component{
      render(){
